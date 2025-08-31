cb99306b401abc306835fbcae32185f0e65fce3a
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2025-08-31 22:31:36 +0800

    Update output value for public subnets module

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 
 output "vpc_public_subnets" {
   description = "IDs of the VPC's public subnets"
-  value       = module.vpc.public_subnets
+  value       = module.hbank-vpc.public_subnets
 }
 
 output "ec2_instance_public_ips" {
